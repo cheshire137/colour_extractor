bbb95fcd3ccd80f15aa1c0fc3cc21ab2e75d1b4d
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-05-04 15:04:53 -0400

    It extracts colors, displays them, lets you create palette

--- extension/colour_extractor.js
+++ extension/colour_extractor.js
@@ -1,5 +1,84 @@
+var colour_extractor = {
+  palette_size: 5,
+
+  color_piece_to_hex: function(piece) {
+    var hex = piece.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;
+  },
+
+  split_rgb_code: function(rgb_code) {
+    var parts = rgb_code.split(', ');
+    var r = parseInt(parts[0].split('(')[1], 10);
+    var g = parseInt(parts[1], 10);
+    var b = parseInt(parts[2].split(')')[0], 10);
+    return [r, g, b];
+  },
+
+  rgb2hex: function(rgb_code) {
+    var parts = this.split_rgb_code(rgb_code);
+    var r = parts[0], g = parts[1], b = parts[2];
+    return '#' + this.color_piece_to_hex(r) + this.color_piece_to_hex(g) +
+           this.color_piece_to_hex(b);
+  },
+
+  has_color: function(rgb_code) {
+    if (rgb_code.substring(0, 4) == 'rgb(') {
+      return true;
+    }
+    return rgb_code.substring(0, 5) == 'rgba(' &&
+           rgb_code.substring(rgb_code.length - 3) != ' 0)';
+  },
+
+  get_largest_color_areas: function(color_areas) {
+    var tuples = [];
+    for (var color in color_areas) {
+      tuples.push([color, color_areas[color]]);
+    }
+    tuples.sort(function(a, b) {
+      a = a[1];
+      b = b[1];
+      return a < b ? 1 : (a > b ? -1 : 0);
+    });
+    var top_colors = [];
+    var limit = Math.min(tuples.length, this.palette_size);
+    for (var i=0; i<limit; i++) {
+      var color = tuples[i][0];
+      top_colors.push(this.rgb2hex(color));
+    }
+    return top_colors;
+  },
+
+  extract_colors: function(callback) {
+    var me = this;
+    var color_areas = {};
+    var add_to_hash = function(key, value) {
+      if (color_areas.hasOwnProperty(key)) {
+        color_areas[key] = color_areas[key].concat([value]);
+      } else {
+        color_areas[key] = [value];
+      }
+    };
+    $('*').filter(':visible').each(function() {
+      var el = $(this);
+      var area = el.width() * el.height();
+      var bg_color = el.css('background-color');
+      if (me.has_color(bg_color)) {
+        add_to_hash(bg_color, area);
+      }
+    });
+    callback(this.get_largest_color_areas(color_areas));
+  },
+
+  on_popup_opened: function(callback) {
+    this.extract_colors(callback);
+  }
+};
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.greeting == 'popup_opened') {
-    // TODO: extract colors from page, send them in response to popup
+    colour_extractor.on_popup_opened(function(colors) {
+      console.log(colors);
+      sendResponse(colors);
+    });
   }
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,41 @@
+var extractor_popup = {
+  display_colors: function(colors) {
+    for (var i=0; i<colors.length; i++) {
+      $('#color' + (i+1)).css('background-color', colors[i]);
+    }
+  },
+
+  strip_pound_sign: function(hex_codes) {
+    var stripped = [];
+    for (var i=0; i<hex_codes.length; i++) {
+      stripped.push(hex_codes[i].replace(/^#/, ''));
+    }
+    return stripped;
+  },
+
+  set_create_link: function(colors) {
+    var url = 'http://www.colourlovers.com/palettes/add?colors=' +
+              this.strip_pound_sign(colors).join(',');
+    $('#create-palette').unbind('click').click(function() {
+      chrome.tabs.create({url: url});
+      return false;
+    });
+  },
+
+  on_colors_extracted: function(colors) {
+    this.display_colors(colors);
+    this.set_create_link(colors);
+  }
+};
+
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.sendRequest(
       tab.id,
-      {greeting: 'popup_opened', tab_id: tab.id},
-      function() {
-        // TODO: display colors extract from page in the popup
+      {greeting: 'popup_opened'},
+      function(colors) {
+        extractor_popup.on_colors_extracted(colors);
       }
     );
-  }
+  });
 });
