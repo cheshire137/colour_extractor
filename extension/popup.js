6b07a74e6b21199aad4530278adad87a46466d02
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-05-12 17:03:09 -0400

    Adding Google Analytics tracking code
    Also adding .gitignore.

--- extension/popup.js
+++ extension/popup.js
@@ -15,6 +15,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40846020-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var extractor_popup = {
   palette_size: 5,
 
